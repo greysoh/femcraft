a664b2a0bac483f7beb58789aef2134eb099fa70
greysoh <greysoh@pm.me>
2022-04-09 21:57:48 -0400

    fix: Updates out of date packages.

--- src/log4/mod.rs
+++ src/log4/mod.rs
@@ -1,9 +1,9 @@
-extern crate time;
-
+extern crate chrono;
 use std::assert;
-use time::now;
 
 pub fn log(type_of: &str, thread_type: &str, message: &str) {
+    let time = chrono::DateTime::time(&chrono::offset::Local::now());
     assert!(type_of == "info" || type_of == "warn" || type_of == "error");
-    println!("({}:{}:{}) [{} in {}] {}", now().tm_hour, now().tm_min, now().tm_sec, type_of, thread_type, message);
+    
+    println!("({}) [{} in {}] {}", time, type_of, thread_type, message);
 }
