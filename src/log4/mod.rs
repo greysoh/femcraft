658781d12fcf6803316f05a49086845268beb215
ashe <rosascript@gmail.com>
2022-04-10 21:42:47 +0000

    add .editorconfig

--- src/log4/mod.rs
+++ src/log4/mod.rs
@@ -4,6 +4,6 @@ use std::assert;
 pub fn log(type_of: &str, thread_type: &str, message: &str) {
     let time = chrono::DateTime::time(&chrono::offset::Local::now());
     assert!(type_of == "info" || type_of == "warn" || type_of == "error");
-    
+
     println!("({}) [{} in {}] {}", time, type_of, thread_type, message);
 }
