4f3946a10491ec58e3a67b5f69c800bb71cf2174
ashe <rosascript@gmail.com>
2022-04-22 17:23:38 -0400

    implement get_value function

--- src/config_parser/mod.rs
+++ src/config_parser/mod.rs
@@ -1,8 +1,19 @@
+//! config parser for minecraft server.properties files
+
+/** accepts a server.properties file as a '&str' and will return a 'Vec<&str>' in a format acceptable by all functions that manipulate it **/
 pub fn get_config(config: &str) -> Vec<&str> {
     let configuration: Vec<&str> = config.trim().split("\n").filter(|x| !x.starts_with("#")).collect();
     return configuration;
 }
 
-pub fn get_value(config: Vec<&str>) -> String {
-    todo!("figure out how to parse the vec");
+/// gets a value from a valid config 'Vec<&str>'
+/// returns an empty string when a option doesn't have a value
+pub fn get_value(config: Vec<&str>, key: &str) -> String {
+    for value in config {
+        let parsed: Vec<&str> = value.split("=").collect();
+        if parsed[0] == key {
+            return parsed[1].to_string()
+        }
+    }
+    return "".to_string(); // use empty string when error
 }
