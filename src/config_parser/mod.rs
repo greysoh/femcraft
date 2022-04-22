c03ff82994900724b9b8b46811fd15f2923cb6cd
ashe <rosascript@gmail.com>
2022-04-22 16:40:21 -0400

    create value parser that only accepts one value currently

--- src/config_parser/mod.rs
+++ src/config_parser/mod.rs
@@ -1,1 +1,15 @@
-pub fn get_value(config: &str, key: &str) -> String { return "" }
+pub fn get_value(config: &str, key: &str) -> String {
+    let mut found_value = false;
+    for token in config.trim().split("=") {
+        if found_value {
+            return token.to_string();
+        }
+        if token.starts_with("#") {
+            continue;
+        }
+        if token == key {
+           found_value = true;
+        }
+    }
+    return "".to_string();
+}
