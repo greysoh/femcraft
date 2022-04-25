e5d2ee96523aacb5b4a5a3758c6a29065b1b3a51
ashe <rosascript@gmail.com>
2022-04-25 00:17:48 +0000

    format config_parser

--- src/config_parser/mod.rs
+++ src/config_parser/mod.rs
@@ -1,9 +1,13 @@
 //! config parser for minecraft server.properties files
 
-/** accepts a server.properties file as a '&str' and will return a 'Vec<&str>' in a format acceptable by all functions that manipulate it **/
+/** accepts a server.properties file as a '&str' and will return a 'Vec<&str>' in a format acceptable by all functions that manipulate it * */
 pub fn get_config(config: &str) -> Vec<&str> {
-    let configuration: Vec<&str> = config.trim().split("\n").filter(|x| !x.starts_with("#")).collect();
-    return configuration;
+    let configuration: Vec<&str> = config
+        .trim()
+        .split("\n")
+        .filter(|x| !x.starts_with("#"))
+        .collect();
+    return configuration
 }
 
 /// gets a value from a valid config 'Vec<&str>'
@@ -15,5 +19,5 @@ pub fn get_value(config: Vec<&str>, key: &str) -> String {
             return parsed[1].to_string()
         }
     }
-    return "".to_string(); // use empty string when error
+    return "".to_string() // use empty string when error
 }
